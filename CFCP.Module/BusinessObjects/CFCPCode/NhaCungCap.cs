7a51fb0b3bd676454650edfe5b3f1a42f433db44
vominhchuong219-a11y <vominhchuong219@gmail.com>
2025-11-24 13:08:20 +0700

    Add project files.

---
+++ CFCP.Module/BusinessObjects/CFCPCode/NhaCungCap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace CFCP.Module.BusinessObjects.CFCP
+{
+
+    public partial class NhaCungCap
+    {
+        public NhaCungCap(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ CFCP.Module/BusinessObjects/CFCPCode/PhieuNhap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace CFCP.Module.BusinessObjects.CFCP
+{
+
+    public partial class PhieuNhap
+    {
+        public PhieuNhap(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
